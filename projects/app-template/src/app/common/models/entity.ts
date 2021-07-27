a8190f2d74e888831461dfbe1d4acb1231e708f4
smileazeez1711 <88017150+smileazeez1711@users.noreply.github.com>
2021-07-27 09:59:56 +0530

    Add files via upload

---
+++ projects/app-template/src/app/common/models/entity.ts
@@ -0,0 +1,11 @@
+import { EntityMetaData, FieldType } from "@esaf-platform-client/fwk-core";
+
+export class Entity
+{
+    EntityMetaData: Array<EntityMetaData>;
+}
+
+export interface IEntity
+{
+
+}
